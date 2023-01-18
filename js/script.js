c323e63b61fb2eedda5ef6f9b82bae51ef7e35fe
Amit Kumar Mishra <31859346+mishracodes@users.noreply.github.com>
2023-01-18 17:39:08 +0530

    fixed some issues

--- js/script.js
+++ js/script.js
@@ -1,62 +1,76 @@
-var startDate = new Date("1998-09-10");
-var diffDate = new Date(new Date() - startDate);
-document.getElementById("dobdiff2").innerHTML=((diffDate.toISOString().slice(0, 4) - 1970));
-document.getElementById("dobdiff").innerHTML=((diffDate.toISOString().slice(0, 4) - 1970));
+const startDate = new Date("1998-09-10");
+const diffDate = new Date(new Date() - startDate);
+document.getElementById("dobdiff2").innerHTML =diffDate.toISOString().slice(0, 4) - 1970;
+document.getElementById("dobdiff").innerHTML =diffDate.toISOString().slice(0, 4) - 1970;
 
-const statsText = document.querySelectorAll('.stats-count');
-statsText.forEach((element)=>{
-        let interval=setInterval(function() 
-                        { 
-                                let count = parseInt(element.innerText);
-                                const target=parseInt(element.getAttribute('data-target'));;
-                                if(count>=target) clearInterval(interval); 
-                               // const increment = (target / speed);
-                                element.innerText = count+2;
-                        }, 50);
-})
+const statsText = document.querySelectorAll(".stats-count");
+const aboutSection = document.getElementById("aboutme");
+let statDone = false;
+const obsCallback = function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.intersectionRatio > 0.5 && statDone === false) {
+      statsText.forEach((element) => {
+        let interval = setInterval(function () {
+          let count = parseInt(element.innerText);
+          const target = parseInt(element.getAttribute("data-target"));
+          if (count >= target) clearInterval(interval);
+          // const increment = (target / speed);
+          element.innerText = count + 2;
+        }, 10);
+      });
 
-const bars = document.querySelectorAll('.bar');
-
-bars.forEach((element)=>{
-	var width = 10;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= parseInt(element.getAttribute('data-target'))) {
-        clearInterval(id);
-      } else {
-        width++;
-        element.style.width = width + "%";
-      }
+      statDone = true;
     }
-}
-)
+  });
+};
 
+const obsOptions = {
+  root: null,
+  threshold: 0.6,
+};
 
-let modalBox = document.getElementById("modal");
-let modalBtn = document.querySelectorAll('.modalOpen');
-let modalSpan = document.getElementsByClassName("modalClose")[0];
-modalBtn.forEach((element)=>{
-  element.onclick = function() {
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(aboutSection);
+
+const bars = document.querySelectorAll(".bar");
+
+bars.forEach((element) => {
+  let width = 10;
+  const id = setInterval(frame, 10);
+  function frame() {
+    if (width >= parseInt(element.getAttribute("data-target"))) {
+      clearInterval(id);
+    } else {
+      width++;
+      element.style.width = width + "%";
+    }
+  }
+});
+
+const modalBox = document.getElementById("modal");
+const modalBtn = document.querySelectorAll(".modalOpen");
+const modalSpan = document.getElementsByClassName("modalClose")[0];
+modalBtn.forEach((element) => {
+  element.onclick = function () {
     modalBox.style.display = "block";
     document.getElementById("modalMainImage").src = element.currentSrc;
-  }
-  modalSpan.onclick = function() {
+  };
+  modalSpan.onclick = function () {
     modalBox.style.display = "none";
-  }
-  window.onclick = function(event) {
+  };
+  window.onclick = function (event) {
     if (event.target == modalBox) {
       modalBox.style.display = "none";
     }
-  }
-})
+  };
+});
 
-var msgForm = document.getElementById("contact-form");
-function handleForm(event) { event.preventDefault();
-  document.getElementById("cfsubmit").value="Submitted Successfully!!!";
-  setTimeout(()=>{
-    document.getElementById("cfsubmit").value="Submit";
-  }, 5000);
-
-} 
-  msgForm.addEventListener('submit', handleForm);
-
+const msgForm = document.getElementById("contact-form");
+function handleForm(event) {
+  event.preventDefault();
+  document.getElementById("cfsubmit").value = "Submitted Successfully!!!";
+  setTimeout(() => {
+    document.getElementById("cfsubmit").value = "Submit";
+  }, 3000);
+}
+msgForm.addEventListener("submit", handleForm);
